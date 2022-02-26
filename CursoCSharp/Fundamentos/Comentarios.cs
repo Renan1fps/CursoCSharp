7ce49b226e23d51ff3a87f3c3044c0fe36138e55
Renan <renan.sergipe68@gmail.com>
2022-02-26 18:23:11 -0300

    refactor: code comments

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    class Comentarios {
+        public static void Executar() {
+            // Comentários de uma linha 
+            Console.WriteLine("Executando");
+
+            /*
+             Comentários de varias linhas 
+            */
+            Console.WriteLine("Executando 2");
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -8,6 +8,7 @@ namespace CursoCSharp {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"Primeiro Programa - Fundamentos", Primeiro.Executar},
+                {"Comentarios Programa - Fundamentos", Comentarios.Executar},
             });
 
             central.SelecionarEExecutar();
