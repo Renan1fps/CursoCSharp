e214cbe1e4433d9a74fa7926325840083178e01c
Renan <renan.sergipe68@gmail.com>
2022-02-26 20:30:42 -0300

    feat: type inference

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Inferencia {
+        public static void Executar() {
+            // inferencia de tipos
+
+            var nome = "renan"; // devo inicializar
+
+            // nome = 123 // dá erro pois ela foi inicializada com um string
+
+
+        }
+    }
+}
