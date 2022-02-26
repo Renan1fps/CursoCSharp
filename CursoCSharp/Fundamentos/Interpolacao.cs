e4fbfd35556e7b08b1ec42bca068236ba52ef02e
Renan <renan.sergipe68@gmail.com>
2022-02-26 20:58:49 -0300

    feat: interpolation in C#

---
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Interpolacao {
+        public static void Executar() {
+            string nome = "Notebook gamer";
+            string marca = "Dell";
+            double preco = 4500.55;
+
+            Console.WriteLine($"O {nome} da {marca} é muito bom e custa {preco}");
+            Console.WriteLine("Outra forma -> o {0} da {1} é muito bom e custa {2}", nome, marca, preco);
+            Console.WriteLine($"Soma -> 1+1 = {1 + 1}");
+        }
+    }
+}
