73b50d6242149f992c693e5904eb31fbb5c2b0da
Renan <renan.sergipe68@gmail.com>
2022-02-27 14:14:33 -0300

    feat: converts in C#

---
+++ CursoCSharp/Fundamentos/Conversoes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    class Conversoes {
+        public static void Executar() {
+            // conversão implicita
+            int inteiro = 10;
+            double quebrado = inteiro;
+            Console.WriteLine(quebrado);
+
+            // conversão explicita
+            double notaAluno = 9.7;
+            int final = (int)notaAluno;
+
+            Console.WriteLine("Digite sua idade: ");
+            string valor = Console.ReadLine();
+            int parseDoValor = int.Parse(valor);
+
+            // nesse caso vai tentar realizar o parse se for um numero retorna ele, senão retorna zero
+            Console.WriteLine("Digite um numero:");
+            int.TryParse(Console.ReadLine(), out int parseado);
+            Console.WriteLine("O valor parseado é -> {0}", parseado);
+        }
+    }
+}
